cc1005be663fa581052ef59d285fa5c90ea6115b
Razdan <razdan@curaweda.id>
2025-08-14 20:11:56 +0700

    add loading screen

---
+++ src/components/Loading.tsx
@@ -0,0 +1,13 @@
+
+const Loading = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+        <div>
+            <span className="text-xl animate-pulse">Loading..</span>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Loading
